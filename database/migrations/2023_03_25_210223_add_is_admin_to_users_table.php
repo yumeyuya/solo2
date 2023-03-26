04028d3836400d120a943ec4a99d8acbdc2a5ecf
yumeyuya <yuya36senbonzakura@gmail.com>
2023-03-26 15:37:42 +0900

    Procfile

---
+++ database/migrations/2023_03_25_210223_add_is_admin_to_users_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIsAdminToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_admin')->default(false);
+        });
+        DB::table('users')->insert([
+            'name' => 'Admin User',
+            'email' => 'admin33@example.com',
+            'password' => bcrypt('24681234'),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('is_admin');
+        });
+    }
+}
